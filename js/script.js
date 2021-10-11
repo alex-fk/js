1a5336c63f2497d781fa0dad6eec437ccfa80ec6
Alex <alex_fk@bk.ru>
2021-10-11 11:59:07 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use sctrict";
+
+let path = 'toys';
+
+console.log(`https://mail.ru/${path}`);
+
+let incr = 10,
+    decr = 20;
+
+    incr++;
+    decr--;
+
+    console.log(incr);
+    console.log(decr);
+
+
