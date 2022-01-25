3f24c87ad5dbff2f2894b31035122a7da32e116b
Alex <alex_fk@bk.ru>
2022-01-25 08:00:28 +0300

    comment commit

---
+++ Подсказка.js
@@ -0,0 +1,22 @@
+"use strict" - новый формат
+alert('Hello');
+const result = confirm('Подтверждение действия с результатом true or false');
+const result = prompt('Подтверждение действия с результатом произвольным','дефолтный ответ -тип данных строка');
+const result = +prompt('Подтверждение действия с результатом произвольным','дефолтный ответ -тип данных число');
+console.log(typeof(result)); узнать тип переменной
+const cat = 'toys';
+console.log(`https://someurl.com/${cat}/5`); - конкатинация
+let incr = 10;
+    dect = 12;  задание нескольких переменных
+incr++; инкремент
+descr--;декримент
+?? - и ,  || - или
+
+------GIT------
+git init - инициализация git
+git config 
+git config --global user.email alex_fk@bk.ru
+git config --global user.name "Alex"
+
+git status - статус GIT
+git add -A - за чем следит GIT
